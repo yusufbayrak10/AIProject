75ca09cb75fffa878ab4da4446bd093e203488fe
yusufbayrak10 <hacibayrak34@gmail.com>
2026-08-06 10:07:38 +0300

    Delete OpenCv_Gray directory

--- OpenCv_Gray/main.py
+++
@@ -1,6 +0,0 @@
-#OpenCV kullanarak açtığınız bu resmi gri tonlamaya dönüştürün. (Filtre uygulansın.)
-import cv2
-
-image = cv2.imread("images.jpg")
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cv2.imwrite("gray.jpg", gray)
