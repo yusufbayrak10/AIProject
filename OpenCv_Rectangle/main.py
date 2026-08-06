0d13853d24a20819f70ad49d884a7d3278f862b3
yusufbayrak10 <hacibayrak34@gmail.com>
2026-08-06 10:28:30 +0300

    Delete OpenCv_Rectangle directory

--- OpenCv_Rectangle/main.py
+++
@@ -1,7 +0,0 @@
-#Resmin belirli bir kısmını sarı renkli bir çerçeveyle kare veya dikdörtgen içine alın. Bu alanın içini doldurun.
-
-import cv2
-image = cv2.imread("images.jpg")
-cv2.rectangle(image, (50, 50), (150, 150), (128, 0, 0), -1)
-cv2.rectangle(image, (50, 50), (150, 150), (0, 255, 255), 2)
-cv2.imwrite("rectangle.jpg", image)
